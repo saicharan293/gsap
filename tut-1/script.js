5d741b79fefa75b426d093cbd7863bef803fb552
saicharan293 <2493saicharan@gmail.com>
2024-10-01 13:12:26 +0530

    gsap to and from and timeline is done

---
+++ tut-1/script.js
@@ -0,0 +1,45 @@
+// gsap.to("#box1",{
+//     x:1000,
+//     rotate: 360,
+//     duration: 1.5,
+//     delay:1
+// }
+// )
+
+// gsap.to('#box2',{
+//     x:1000,
+//     backgroundColor:"yellow",
+//     duration:1.5,
+//     delay:2.5
+// })
+
+// gsap.to('#box3',{
+//     x:1000,
+//     duration:1.5,
+//     delay:4,
+//     borderRadius:"50%",
+//     rotate:90
+// })
+
+// var tl=gsap.timeline();
+
+// tl.to("#box1",{
+//     x:1000,
+//     rotate:360,
+//     duration:1.5,
+//     delay:1
+// })
+
+// tl.to("#box2",{
+//     x:1000,
+//     duration:1.5,
+//     delay:1
+// })
+
+// tl.to("#box3",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:90,
+//     borderRadius:"50%"
+// })
