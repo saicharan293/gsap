6aa085793bcf340b341626dd3660d1600b785938
saicharan293 <2493saicharan@gmail.com>
2024-10-05 17:10:29 +0530

    det added to x point for dynamic approach

--- tut-3/script.js
+++ tut-3/script.js
@@ -5,7 +5,7 @@ var finalPath = `M 10 100 Q 500 100 990 100`;
 var string = document.querySelector('#string');
 
 string.addEventListener('mousemove',function(dets){
-    initialPath = `M 10 100 Q 500 ${dets.y} 990 100`,
+    initialPath = `M 10 100 Q ${dets.x} ${dets.y} 990 100`,
     gsap.to("svg path", {
         attr:{ d: initialPath },
         duration: 0.3,
