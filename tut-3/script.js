99f58e718916237bee83aef82076adfd42a4790e
saicharan293 <2493saicharan@gmail.com>
2024-10-05 17:04:22 +0530

    svg motion using gsap is cone

---
+++ tut-3/script.js
@@ -0,0 +1,23 @@
+var initialPath = `M 10 100 Q 500 100 990 100`;
+
+var finalPath = `M 10 100 Q 500 100 990 100`;
+
+var string = document.querySelector('#string');
+
+string.addEventListener('mousemove',function(dets){
+    initialPath = `M 10 100 Q 500 ${dets.y} 990 100`,
+    gsap.to("svg path", {
+        attr:{ d: initialPath },
+        duration: 0.3,
+        ease:"power3.out"
+    });
+});
+
+
+string.addEventListener('mouseleave',function(){
+    gsap.to("svg path", {
+        attr: { d: finalPath},
+        duation:2.5,
+        ease:"elastic.out(1,0.2)"
+    });
+})
